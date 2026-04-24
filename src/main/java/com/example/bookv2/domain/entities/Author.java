db6344e64ccf0c7b508ad0eaecf4d971189aa3ce
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2026-04-25 02:17:16 +0400

    create new table author

---
+++ src/main/java/com/example/bookv2/domain/entities/Author.java
@@ -0,0 +1,18 @@
+package com.example.bookv2.domain.entities;
+
+import com.example.bookv2.domain.entities.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author extends BaseEntity {
+    private String Name;
+}
